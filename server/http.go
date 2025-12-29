be6608052cf1affda3f1e9c2b2ede1911ec6becb
Martin Maartensson <martinmaartensson@gmail.com>
2025-12-29 08:09:50 +0100

    update function signature

--- server/http.go
+++ server/http.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net"
@@ -11,23 +12,46 @@ import (
 
 func HTTP(
 	ctx context.Context,
-	cancel context.CancelFunc,
-	handler http.Handler,
 	log *slog.Logger,
 	ln net.Listener,
-	idleTimeout time.Duration,
-	connContext func(ctx context.Context, c net.Conn) context.Context,
+	handler http.Handler,
+	opts ...HTTPOption,
 ) error {
+	if ctx == nil {
+		return errors.New("nil context")
+	}
+	if log == nil {
+		return errors.New("nil logger")
+	}
+	if ln == nil {
+		return errors.New("nil listener")
+	}
+	if handler == nil {
+		return errors.New("nil handler")
+	}
+
+	cfg := defaultHTTPConfig()
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var connStateFn func(net.Conn, http.ConnState)
+	if cfg.idleTimeout > 0 {
+		connStateFn = idleTracker(ctx, cancel, cfg.idleTimeout)
+	}
 
 	srv := &http.Server{
 		Handler:           handler,
 		BaseContext:       func(ln net.Listener) context.Context { return ctx },
-		ConnContext:       connContext,
+		ConnContext:       cfg.connContext,
 		ReadTimeout:       15 * time.Second,
 		ReadHeaderTimeout: 10 * time.Second,
 		WriteTimeout:      0, // MUST be 0 for streaming
 		IdleTimeout:       0,
-		ConnState:         idleTracker(ctx, cancel, idleTimeout),
+		ConnState:         connStateFn,
 		ErrorLog:          slog.NewLogLogger(log.Handler(), slog.LevelError),
 	}
 	defer srv.Close()
@@ -46,7 +70,7 @@ func HTTP(
 
 	log.Warn("shutting down server")
 
-	shutdownCtx, shutDownCancel := context.WithTimeout(ctx, 5*time.Second)
+	shutdownCtx, shutDownCancel := context.WithTimeout(ctx, cfg.shutdownTimeout)
 	defer shutDownCancel()
 
 	if err := srv.Shutdown(shutdownCtx); err != nil {
@@ -56,4 +80,38 @@ func HTTP(
 	log.Warn("server successfully shut down")
 
 	return nil
+}
+
+type HTTPOption func(*httpConfig)
+
+type httpConfig struct {
+	idleTimeout     time.Duration
+	shutdownTimeout time.Duration
+	connContext     func(context.Context, net.Conn) context.Context
+}
+
+func defaultHTTPConfig() httpConfig {
+	return httpConfig{
+		shutdownTimeout: 5 * time.Second,
+	}
+}
+
+func WithShutdownOnIdle(d time.Duration) HTTPOption {
+	return func(c *httpConfig) {
+		c.idleTimeout = d
+	}
+}
+
+func WithShutdownTimeout(d time.Duration) HTTPOption {
+	return func(c *httpConfig) {
+		c.shutdownTimeout = d
+	}
+}
+
+func WithConnContext(
+	fn func(context.Context, net.Conn) context.Context,
+) HTTPOption {
+	return func(c *httpConfig) {
+		c.connContext = fn
+	}
 }
